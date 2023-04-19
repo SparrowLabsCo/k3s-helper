83fc93c7e656c449b3224adee5ff9189ac91bc6a
Jon Ceanfaglione <jonc@jons-mbp.lan>
2023-04-19 14:26:24 -0400

    updated k8s helper bash

--- scripts/libs/k8s.sh
+++ scripts/libs/k8s.sh
@@ -6,6 +6,7 @@ wait_for_job(){
 }
 
 wait_for_deployment(){
-    info "Waiting for deployment $1 to complete"
-    kubectl -n $2 wait --for=condition=Available=True --timeout=600s deployment.apps/$1
+    info "Waiting for deployment of $1 to complete"
+    kubectl rollout status deployment $1 -n $2
+    #kubectl -n $2 wait --for=condition=Available=True --timeout=600s deployment.apps/$1
 }
